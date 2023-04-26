9eb9fe7925003a81213c26324ae32a85a66cf46d
Evgeniy <evgnec@gmail.com>
2023-04-26 22:07:03 +0300

    add 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import './css/styles.css';
+import { fetchCountries } from './js/func';
+import { debounce } from 'lodash';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
+
+const searchBoxRef = document.getElementById('search-box');
+const countryListRef = document.getElementById('.country-list');
+const countryInfoRef = document.getElementById('.country-info');
+
+searchBoxRef.addEventListener('input', debounce(searchHandler, DEBOUNCE_DELAY));
+
+function searchHandler(e) {
+    e.preventDefault();
+    const name = searchBoxRef.ariaValueMax.trim();
+
+    if (name) {
+        return fetchCountries(name).then(creatMarkup).catch(error);
+    }
+    else {
+ clearHTML;
+    }
+}
+
+function creatMarkup(countries) {
+    clearHTML;
+    if 
+}
+
+
+
+function clearHTML() {
+       countryListRef.innerHTML = '';
+    countryInfoRef.innerHTML = ''; 
+}
+
